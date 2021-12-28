5be7c71330d4758f865cc8364ecd7197263bcc9a
Guilherme R <guilhaxr123@gmail.com>
2021-12-28 22:08:17 +0000

    doing animations

--- src/App.js
+++ src/App.js
@@ -15,31 +15,6 @@ function App() {
 
 
 
-  // const [ counterAbout, setCounterAbout] = useState(0)
-  // const [ counter2, setCounter2] = useState(0)
-
-
-  // const [imagePos, setImagePos] = useState({x: 0, y: 0});
-  // const [aboutPos, setAboutPos] = useState({x: 0, y: 0});
- 
-  // const bindImagePos = useDrag(({ down, offset: [ox, oy]}) => setImagePos({ x: ox, y: oy, immediate: down }), {
-  //   bounds: {top: 16 ,  left:-Infinity, right:Infinity
-  //   } })
-
-  //   const bindAboutPos = useDrag(({ down, offset: [ox, oy] }) => setAboutPos({ x: ox, y: oy, immediate: down }), {
-  //     bounds: {top: 16 ,  left:-Infinity, right:Infinity
-  //     } })
-
- 
-  //     let incrementAbout = () => {
-  //       setCounterAbout((counterAbout + counter2)  + 2 )
-  //     }
-
-  //     let incrementWindow = () => {
-  //       setCounter2((counterAbout + counter2)  + 2 )
-  //     }
-
-
     const HandlerAbout = () => {
       setOpenAbout(!openAbout)
       
@@ -62,35 +37,6 @@ function App() {
         openAbout={openAbout}
         openWindow={openWindow}
       />
-
-       {/* <div  onClick={incrementAbout}  {...bindAboutPos()}  style={{
-          width: "fit-content",
-          position:"absolute",
-          top: aboutPos.y,
-          left: aboutPos.x,
-          zIndex: counterAbout 
-          
-        }}>
-          { openAbout && <About HandlerButton={HandlerAbout}  />  }
-          
-        </div>
-
-
-
-
-        <div onClick={incrementWindow}  {...bindImagePos()} style={{
-          width: "fit-content",
-          position:"absolute",
-          top: imagePos.y,
-          left: imagePos.x,
-          zIndex: counter2
-
-        }}
-        >
-          {openWindow && <Contact HandlerButton={HandlerWindow}  />}
-          
-        </div> */}
-
 
       <Dock HandlerWindow={HandlerWindow} HandlerAbout={HandlerAbout}/>
       
